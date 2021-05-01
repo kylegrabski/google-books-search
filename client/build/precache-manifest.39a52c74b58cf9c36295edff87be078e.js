e9fda974220187a3242f57adf02e4e6822b88eb5
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-30 21:15:47 -0700

    init commit client build

---
+++ client/build/precache-manifest.39a52c74b58cf9c36295edff87be078e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bd82e6e511366a2b960e06515f387a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d195e43dcb9ee48cf036",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "c0f682c76afcc4fc9015",
+    "url": "/static/js/2.28f0e3e2.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.28f0e3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d195e43dcb9ee48cf036",
+    "url": "/static/js/main.fd566223.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
