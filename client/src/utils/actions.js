13ae95e5766d0ca5824554f526843a81a0fbe22c
Kyle Grabski <kyle.grabski@gmail.com>
2021-05-03 20:37:55 -0700

    code refactor

---
+++ client/src/utils/actions.js
@@ -0,0 +1,4 @@
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+export const UPDATE_FAVORITES = "UPDATE_FAVORITES";
+export const LOADING = "LOADING";
