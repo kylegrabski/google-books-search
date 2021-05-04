736140046435da219651f43d6647f09651d910bf
Kyle Grabski <kyle.grabski@gmail.com>
2021-05-03 20:37:19 -0700

    Add savedbooks route

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavHeader from "./components/Navbar/Navbar";
 import Search from "./pages/Search";
+import SavedBooks from "./pages/SavedBooks"
 
 function App() {
   return (
     <Router>
       <div>
-        <NavHeader />
-        <Route exact path="/" render={()=> <Search/>}/>
+          <NavHeader />
+          <Route exact path="/" render={() => <Search />} />
+          <Route exact path="/saved-books" render={() => <SavedBooks />} />
       </div>
     </Router>
   );
