2c8c3c793b3651a45036ec543ddd93470fc81dfb
Kyle Grabski <kyle.grabski@gmail.com>
2021-05-05 09:30:59 -0700

    added MONGODB_URI

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //COnnect to the Mongo DB
-mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/googlebooks", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
